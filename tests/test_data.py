50e30bab30b7b88182bcd5030f11eae2f3f97167
Willz789 <williammistarz@gmail.com>
2026-01-15 16:04:01 +0100

    Add unit tests for data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,52 @@
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-from cifakeclassification.data import MyDataset
+from cifakeclassification.data import create_data_module, ImageDataModule
 
 
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+def test_class_init():
+    """Test the ImageDataModule class."""
+    datamodule = create_data_module("data")
+    assert isinstance(datamodule, ImageDataModule)
+    assert isinstance(datamodule.generator, torch.Generator)
+
+
+def test_pt_lightning_setup():
+    """Test the setup method of the ImageDataModule class."""
+    datamodule = create_data_module("data", val_split=0.2)
+    datamodule.setup()
+    
+    assert datamodule.num_classes == 2
+    assert datamodule.classes == ["FAKE", "REAL"]
+    
+    assert isinstance(datamodule.train_dataset, Dataset)
+    assert isinstance(datamodule.val_dataset, Dataset)
+    assert isinstance(datamodule.test_dataset, Dataset)
+    
+    val_size = int(100_000 * datamodule.val_split)
+    
+    assert len(datamodule.train_dataset) == 100_000 - val_size
+    assert len(datamodule.val_dataset) == val_size
+    assert len(datamodule.test_dataset) == 20000
+    
+
+def test_dataloaders():
+    """Test the dataloader methods of the ImageDataModule class."""
+    datamodule = create_data_module("data", batch_size=16)
+    datamodule.setup()
+    
+    train_loader = datamodule.train_dataloader()
+    val_loader = datamodule.val_dataloader()
+    test_loader = datamodule.test_dataloader()
+    
+    assert isinstance(train_loader, DataLoader)
+    assert isinstance(val_loader, DataLoader)
+    assert isinstance(test_loader, DataLoader)
+    
+    train_batch = next(iter(train_loader))
+    val_batch = next(iter(val_loader))
+    test_batch = next(iter(test_loader))
+    
+    assert train_batch[0].shape == (16, 3, 32, 32)
+    assert val_batch[0].shape == (16, 3, 32, 32)
+    assert test_batch[0].shape == (16, 3, 32, 32)
