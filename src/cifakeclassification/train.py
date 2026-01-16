32168b79e7c6676fbaa160718261600142ee77c6
ViktorHJ <viktor.hougaard.j@gmail.com>
2026-01-16 15:15:10 +0100

    train W&B

--- src/cifakeclassification/train.py
+++ src/cifakeclassification/train.py
@@ -1,19 +1,42 @@
 from cifakeclassification.model import Cifake_CNN
 from cifakeclassification.data import ImageDataModule
+from pytorch_lightning.loggers import WandbLogger
+from dotenv import load_dotenv
 import torch
 import pytorch_lightning as pl
 import typer
+import wandb
+import os
 
+load_dotenv()
 app = typer.Typer()
 
 
 @app.command()
 def train(batch_size: int = 128, epochs: int = 10) -> None:
-    """Train a model on MNIST."""
+    """Train a model on MNIST with W&B."""
+
+    # W&B config from .env
+    project = os.getenv("WANDB_PROJECT")
+    entity = os.getenv("WANDB_ENTITY")
+
+    wandb_logger = WandbLogger(
+        project=project,
+        entity=entity,
+        log_model=True,
+        save_dir=".",
+    )
+
+    wandb_logger.experiment.config.update(
+        {
+            "batch_size": batch_size,
+            "epochs": epochs,
+        }
+    )
 
     datamodule = ImageDataModule(
         batch_size=batch_size,
-        num_workers=0,
+        num_workers=4,
         val_split=0.2,
     )
 
@@ -24,18 +47,31 @@ def train(batch_size: int = 128, epochs: int = 10) -> None:
         accelerator="auto",
         devices="auto",
         log_every_n_steps=50,
+        logger=wandb_logger,
     )
 
-
-
     trainer.fit(model, datamodule=datamodule)
-    torch.save(model.state_dict(), "models/model.pth")
+
+    # -------------------------
+    # Save + log artifact
+    # -------------------------
+    save_path = "models/model.pth"
+    torch.save(model.state_dict(), save_path)
+
+    artifact = wandb.Artifact("cifake-model", type="model")
+    artifact.add_file(save_path)
+    wandb_logger.experiment.log_artifact(artifact)
+
+    wandb.finish()
+
+    # torch.save(model.state_dict(), "models/model.pth")
 
 
 def main() -> None:
     app()
 
+
 if __name__ == "__main__":
     main()
 
-#testing workflows
+# testing workflows
