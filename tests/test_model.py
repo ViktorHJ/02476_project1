d6ae8b1e9740b64c43ac21119c2819a9c1ba147a
Aslan4012 <aslan11@icloud.com>
2026-01-16 17:28:27 +0100

    added model-test

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,22 @@ from cifakeclassification.model import Cifake_CNN
 import torch
 
 
-def test_model_shape():
+def test_forward_shape():
     model = Cifake_CNN()
     x = torch.rand(2, 3, 32, 32)
-    assert model(x).shape == torch.Size([2, 2])
+    y = model(x)
+    assert y.shape == (2, 2)
+
+
+def test_model_hyperparameters():
+    model = Cifake_CNN(
+        activation_function="relu",
+        dropout_rate=0.3,
+        learning_rate=0.001,
+        optimizer="adam",
+    )
+
+    assert isinstance(model.activation, torch.nn.ReLU)
+    assert model.dropout.p == 0.3
+    assert model.learning_rate == 0.001
+    assert model.optimizer is torch.optim.Adam
