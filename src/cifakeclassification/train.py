588bcd155c133a69ba7786e7a69d7aedf0b80f73
Aslan4012 <aslan11@icloud.com>
2026-01-16 16:18:29 +0100

    added configs and modified train and model to include hydra

--- src/cifakeclassification/train.py
+++ src/cifakeclassification/train.py
@@ -1,41 +1,45 @@
-from cifakeclassification.model import Cifake_CNN
-from cifakeclassification.data import ImageDataModule
+from pathlib import Path
+import hydra
+from omegaconf import DictConfig
 import torch
 import pytorch_lightning as pl
-import typer
+from cifakeclassification.model import Cifake_CNN
+from cifakeclassification.data import ImageDataModule
 
-app = typer.Typer()
+CONFIG_DIR = Path(__file__).resolve().parents[2] / "configs"  # -> 02476_project1/configs
+@hydra.main(version_base=None, config_path=str(CONFIG_DIR), config_name="config")
 
 
-@app.command()
-def train(batch_size: int = 128, epochs: int = 10) -> None:
-    """Train a model on MNIST."""
+
+# @hydra.main(version_base=None, config_path="../../configs", config_name="config")
+def train(cfg: DictConfig) -> None:
+    hp = cfg.hyperparameters
 
     datamodule = ImageDataModule(
-        batch_size=batch_size,
+        batch_size=hp.batch_size,
         num_workers=0,
         val_split=0.2,
     )
 
-    model = Cifake_CNN()
+    model = Cifake_CNN(
+        activation_function=hp.activation_function,
+        dropout_rate=hp.dropout_rate,
+        learning_rate=hp.learning_rate,
+        optimizer=hp.optimizer,
+    )
 
     trainer = pl.Trainer(
-        max_epochs=epochs,
+        max_epochs=hp.epochs,
         accelerator="auto",
         devices="auto",
         log_every_n_steps=50,
     )
 
-
-
     trainer.fit(model, datamodule=datamodule)
-    torch.save(model.state_dict(), "models/model.pth")
 
-
-def main() -> None:
-    app()
+    out_dir = Path(hydra.utils.to_absolute_path("models"))
+    out_dir.mkdir(exist_ok=True)
+    torch.save(model.state_dict(), out_dir / "model.pth")
 
 if __name__ == "__main__":
-    main()
-
-#testing workflows
+    train()
